cb60c4c89874d984cb9732f5d858aa29140a6824
rdvn <rdvn@DESKTOP-KROI95K>
2023-04-14 08:53:46 +0300

    Add project files.

---
+++ Northwind.Bussiness/Concrete/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using Northwind.DataAcces.Concrete;
+using Nortwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Bussiness.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+        public List<Product> GetAll()
+        {
+            //bussiness code
+            
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Northwind.DataAcces/Concrete/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Nortwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAcces.Concrete
+{
+    internal class NorthwindContext:DbContext
+    {
+        public DbSet <Product> Products { get; set; }
+    }
+}
